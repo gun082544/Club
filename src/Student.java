60c924c528aa19bf3a34db24ee4845fe5a42a37d
JJ <yinyin.domo@hotmail.com>
2020-01-15 11:34:30 +0700

    [ADD]Student Code
    Student Code

--- src/Student.java
+++ src/Student.java
@@ -1,13 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author int105
- */
 public class Student {
+    private String StudentName;
+    private String StudentLastName;
+    private int StudentId;
+    private String Faculty;
     
+    public Student(String name,String lastname,int id,String faculty){
+        if(id == this.StudentId){
+            System.out.println("We already have this dude.Nothing Happend");
+        }else
+        this.Faculty = faculty;
+        this.StudentId = id;
+        this.StudentLastName = lastname;
+        this.StudentName = name;
+    }
 }
